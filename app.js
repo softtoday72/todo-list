17f9a96ef58c76fe2b609d0f601491d9a0ea9212
softtoday72 <demonhunter720525@hotmail.com>
2022-01-15 03:16:50 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
 const port = 3000
+
+//這邊的todo-list是mongoose資料庫名稱
+mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
+
+//把connection內容抓下來
+const db = mongoose.connection
+//(on 註冊一個事件監聽器，用來監聽 error 事件有沒有發生)
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+//這邊的once表示監聽器只用一次就會刪掉, 節省資源 , open指的是連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 app.get('/', (req, res) => {
   res.send('hellow world!')
---
+++ models/todo.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+//建立Schema的實例
+const todoSchema = new Schema({
+  //name 是屬性
+  name: {
+    type: Strring, //資料型別:字串
+    required: true, //必填欄位
+  }
+})
+//透過 module.exports 把這個 schema 輸出 ,匯出的時候我們把這份 schema 命名為 Todo
+module.exports = mongoose.model('Todo', todoSchema)
