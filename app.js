207466d04bd41c683f2eeaa56cf4e60f1665d31f
softtoday72 <demonhunter720525@hotmail.com>
2022-01-16 17:23:39 +0800

    feat: add delete todo function

--- app.js
+++ app.js
@@ -79,7 +79,13 @@ app.post('/todos/:id/edit', (req, res) => {
     .catch(error => console.log(error))
 })
 
-
+app.post('/todos/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .then(todo => todo.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
 
 app.listen(port, () => {
   console.log(`App is running on http://localhost:${port}`)
