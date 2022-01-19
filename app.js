0ffbb454305d9387489d6f4c4313687bf7439c80
softtoday72 <demonhunter720525@hotmail.com>
2022-01-20 00:30:38 +0800

    feat: add sort function

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ db.once('open', () => {
 app.get('/', (req, res) => {
   Todo.find() //取出 Todo model 裡的所有資料
     .lean()   //把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .sort({ _id: 'asc' }) //asc從小到大 , desc反過來
     .then(todos => res.render('index', {todos: todos}))
     .catch(error => console.log(error))
 })
